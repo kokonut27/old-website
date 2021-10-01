8c57cca5a1ee3166d5a4480933fddd779bbdae05
an aspirin <76911308+JBYT27@users.noreply.github.com>
2021-10-01 14:03:43 +0000

    Create basic files and updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 from flask import *
 
 app = Flask(__name__,
@@ -6,7 +7,30 @@ app = Flask(__name__,
             static_folder='static',
             template_folder='templates')
 
+route = os.environ["secret"]
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+  return render_template(
+    "index.html",
+    page="Home")# make page differnt for every page, use base page and append from there.
+
+@app.route('/about')
+def about():
+  return render_template(
+    "about.html",
+    page="About"
+  )
+
+@app.route(f"/{route}")
+def secret_page():
+  return render_template(
+    "")
+
+@app.errorhandler(404)
+def not_found(e):
+  return render_template(
+    "404.html",
+    page="404 Error")
+
+app.run(host="0.0.0.0", port=8080)
